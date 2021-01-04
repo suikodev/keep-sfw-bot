0db346c519c52eeb351d7ed35b2e7d4e25c68f42
Nacht <nacht42@riseup.net>
2021-01-04 14:08:04 +0800

    feat(*): use dotenv to configure environment variables

---
+++ src/utils/secrets.ts
@@ -0,0 +1,12 @@
+import { logger } from "./logger";
+import dotenv from "dotenv";
+import fs from "fs";
+
+if (fs.existsSync(".env")) {
+  logger.debug("Using .env file to supply config environment variables");
+  dotenv.config({ path: ".env" });
+} else {
+  logger.error(
+    "please create .env file to supply config environment variables"
+  );
+}
