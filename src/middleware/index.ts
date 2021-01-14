57a16dcbc625df969e815f2f35b68c309a5a6b19
Nacht <nacht42@riseup.net>
2021-01-15 02:09:14 +0800

    feat(middleware): add function to store file link in context when updates contain file

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,7 +1,9 @@
 import messageLogger from "./messageLogger";
+import storeFileLink from "./storeFileLink";
 
 const middleware = {
   messageLogger,
+  storeFileLink,
 };
 
 export default middleware;
