c176f89232934a13d97fb4a47edd9e73fbdd248b
Nacht <nacht42@riseup.net>
2021-01-15 13:22:58 +0800

    refactor(src/middleware): reduce storeFileLink function's code line

--- src/middleware/storeFileLink.ts
+++ src/middleware/storeFileLink.ts
@@ -3,21 +3,11 @@ import SFWContext from "../context";
 
 const storeFileLink: Middleware<SFWContext> = async (ctx, next) => {
   let fileId;
-  if ("photo" in ctx.message) {
-    fileId = ctx.message.photo[0].file_id;
-  }
-  if ("video" in ctx.message) {
-    fileId = ctx.message.video.file_id;
-  }
-  if ("sticker" in ctx.message) {
-    fileId = ctx.message.sticker.file_id;
-  }
-  if ("document" in ctx.message) {
-    fileId = ctx.message.document.file_id;
-  }
-  if (fileId) {
-    ctx.fileLink = await ctx.telegram.getFileLink(fileId);
-  }
+  if ("photo" in ctx.message) fileId = ctx.message.photo[0].file_id;
+  if ("video" in ctx.message) fileId = ctx.message.video.file_id;
+  if ("sticker" in ctx.message) fileId = ctx.message.sticker.file_id;
+  if ("document" in ctx.message) fileId = ctx.message.document.file_id;
+  if (fileId) ctx.fileLink = await ctx.telegram.getFileLink(fileId);
   next();
 };
 
