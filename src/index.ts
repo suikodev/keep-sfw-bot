2bb96930f3cd9dae694ef583f3e32837bba0fe61
Nacht <nacht42@riseup.net>
2021-01-17 02:28:03 +0800

    feat(index): add possiblility to launch bot in webhook mode, enable graceful stop

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,22 @@ bot.use(middleware.storeFileId);
 bot.use(middleware.NSFWClassify);
 bot.use(middleware.NSFWUpdateHandler);
 
-bot.launch().then(() => logger.debug("bot launched successfully"));
+if (process.env.NODE_ENV === "production" && process.env.BOT_WEBHOOK_DOMAIN) {
+  bot
+    .launch({
+      webhook: {
+        domain: process.env.BOT_WEBHOOK_DOMAIN,
+        port: parseInt(process.env.BOT_WEBHOOK_PORT, 10) || 3000,
+      },
+    })
+    .then(() =>
+      logger.debug(
+        `use webhook mode, listen at ${process.env.BOT_WEBHOOK_PORT || 3000}`
+      )
+    );
+} else {
+  bot.launch().then(() => logger.debug("use polling mode"));
+}
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
