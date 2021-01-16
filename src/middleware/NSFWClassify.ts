0a711e7e82c57869dcf408842abab2da5f76cc8b
Nacht <nacht42@riseup.net>
2021-01-16 16:31:37 +0800

    feat(middleware): enable production mode before loading NSFWJS model

--- src/middleware/NSFWClassify.ts
+++ src/middleware/NSFWClassify.ts
@@ -1,12 +1,13 @@
 import SFWContext from "../context";
 import { Middleware } from "telegraf";
 import { load as loadNSFWModel, NSFWJS } from "nsfwjs";
-import { node as tfNode, Tensor3D } from "@tensorflow/tfjs-node";
+import * as tf from "@tensorflow/tfjs-node";
 import fetch from "node-fetch";
 import sharp from "sharp";
 
 let model: NSFWJS;
 (async () => {
+  tf.enableProdMode();
   model = await loadNSFWModel("file://model/", { size: 299 });
 })();
 
@@ -14,7 +15,7 @@ const NSFWClassify: Middleware<SFWContext> = async (ctx, next) => {
   const resp = await fetch(ctx.fileLink);
   let imageBuffer = await resp.buffer();
   imageBuffer = await sharp(imageBuffer).jpeg().toBuffer();
-  const image = tfNode.decodeImage(imageBuffer, 3) as Tensor3D;
+  const image = tf.node.decodeImage(imageBuffer, 3) as tf.Tensor3D;
   const predictions = await model.classify(image);
   ctx.predictions = predictions;
   next();
